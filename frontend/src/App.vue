7fce93a70adf90cab41cc7187831e32550819d93
Marques Bruno <bruno.coimbra@protonmail.com>
2021-07-22 22:57:42 +0200

    added a header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <div id="app">
+    <Header />
     <Apartments />
   </div>
 </template>
 
 <script>
 import Apartments from './components/Apartments'
+import Header from './components/Header.vue';
 
 export default {
   name: "App",
   components: {
-    Apartments,
+    Header,
+    Apartments
   },
 };
 </script>
 
 <style src="./assets/tailwind.css"/>
 <style>
-body {
-  background-image: url('https://images.unsplash.com/photo-1496163290419-406e5dd84a07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="inset-x-0 top-0 bg-white shadow-lg">
+    <div class="md:flex items-center justify-between py-2 px-8 md:px-12">
+      <div class="flex justify-between items-center">
+        <div class="text-2xl font-bold text-gray-800 md:text-3xl">
+          <a href="#">RentHunter</a>
+        </div>
+        <div>
+            <a
+          href="#"
+          class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
+          >Apartments</a
+        >
+        <a
+          href="#"
+          class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
+          >Houses</a
+        >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Apartments",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      apartments: [],
+    };
+  },
+  async created() {
+    var response = await fetch("http://127.0.0.1:8000/api/apartments/");
+    this.apartments = await response.json();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
