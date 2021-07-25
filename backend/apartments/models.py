0852617f6b2a76026ef0dc6fc2ebe7df8d7cc5b7
Marques Bruno <bruno.coimbra@protonmail.com>
2021-07-25 16:35:10 +0200

    aded new models

--- backend/apartments/models.py
+++ backend/apartments/models.py
@@ -4,7 +4,20 @@ from django.db import models
 
 class Apartment(models.Model):
     title = models.CharField(max_length=256)
-    price = models.BigIntegerField()
+    image = models.URLField(max_length = 200, default="")
+    price = models.IntegerField(default=0)
+    location = models.CharField(max_length=256, default="")
+    city = models.CharField(max_length=256, default="")
+    description = models.TextField(default="")
+    living_area = models.IntegerField(default=0)
+    rooms = models.IntegerField(default=0)
+    dwelling_type = models.CharField(max_length=256, default="")
 
     class Meta:
         ordering = ['title']
+
+    def __str__(self):
+        return self.title    
+    
+    class Admin:
+        pass
