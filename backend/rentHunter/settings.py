4d929068c0a100e420195033f1f216ea216c0ce4
Marques Bruno <bruno.coimbra@protonmail.com>
2021-07-25 19:54:12 +0200

    added the connection to postresql

--- backend/rentHunter/settings.py
+++ backend/rentHunter/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'rentHunter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'renthunter',
+        'USER': 'brumarq',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
